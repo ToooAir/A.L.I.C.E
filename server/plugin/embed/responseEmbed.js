f37d42157ccf4a1079375b2760e71da1ecc332a7
ToooAir <as134679258@gmail.com>
2022-06-03 16:03:22 +0800

    update: addFields

--- server/plugin/embed/responseEmbed.js
+++ server/plugin/embed/responseEmbed.js
@@ -7,7 +7,7 @@ function help(){
         .setTitle('神聖語指令')
         .setAuthor({name:'愛麗絲', iconURL:'https://i.imgur.com/8hLIxzh.jpg'})
         .setDescription('這是目前我所知道的神聖語')
-        .addFields(
+        .addFields([
             {name:'自我介紹', value: prefixM + 'about'},
             {name:'群組人數', value: prefixM + 'guild'},
             {name:'回覆', value: prefixM + 'reply'},
@@ -15,7 +15,7 @@ function help(){
             {name:'給 一個人 身分組', value: prefixM + 'give'},
             {name:'給 一個身分 身分組', value: prefixM + 'role'},
             {name:'討論區水量排序(with CSV)', value: prefixM + 'rank'},
-        )
+        ])
         .setTimestamp()
 
     return embed;
@@ -28,13 +28,13 @@ function replyHelp(){
         .setAuthor({name:'愛麗絲', iconURL:'https://i.imgur.com/8hLIxzh.jpg'})
         .setDescription('真受不了.....讓我來教你吧')
         .setThumbnail('https://i.imgur.com/pVYFvSs.jpg')
-        .addFields(
+        .addFields([
             {name:'新增/修改回覆', value: prefixM + 'reply add [觸發關鍵字] [回覆內容]'},
             {name:'刪除回覆', value: prefixM + 'reply delete [觸發關鍵字]'},
             {name:'搜尋回覆', value: prefixM +'reply search [觸發關鍵字]'},
             {name:'隨機回覆內容', value: '將多個[回覆內容]用 ^ 來分開'},
             {name:'Tag 觸發訊息者', value: '在[回覆內容]裡加入 {m}'},
-        )
+        ])
         .setTimestamp()
         .setFooter({text:'愛麗絲的神聖語補習教室', iconURL:'https://i.imgur.com/8hLIxzh.jpg'});
     
@@ -48,10 +48,10 @@ function replyAdd(input,output,user){
         .setTitle('回覆已新增')
         .setAuthor({name:'愛麗絲', iconURL:'https://i.imgur.com/8hLIxzh.jpg'})
         .setDescription('明明是騎士卻要講這些意義不明的話')
-        .addFields(
+        .addFields([
             {name:'觸發關鍵字', value: input},
             {name:'回覆內容', value: output ,inline: true}
-        )
+        ])
         .setTimestamp()
         .setFooter({text:user.tag, iconURL:user.avatarURL()});
     
@@ -64,10 +64,10 @@ function replyUpdate(input,output,user){
         .setTitle('回覆已修改')
         .setAuthor({name:'愛麗絲', iconURL:'https://i.imgur.com/8hLIxzh.jpg'})
         .setDescription('你們到底又想要我說些什麼......?')
-        .addFields(
+        .addFields([
             {name:'觸發關鍵字', value: input},
             {name:'回覆內容', value: output ,inline: true}
-        )
+        ])
         .setTimestamp()
         .setFooter({text:user.tag, iconURL:user.avatarURL()});
 
@@ -80,9 +80,9 @@ function replyDelete(input,user){
         .setTitle('回覆已刪除')
         .setAuthor({name:'愛麗絲', iconURL:'https://i.imgur.com/8hLIxzh.jpg'})
         .setDescription('奇怪的話又少了一句')
-        .addFields(
+        .addFields([
             {name:'觸發關鍵字', value: input},
-        )
+        ])
         .setTimestamp()
         .setFooter({text:user.tag, iconURL:user.avatarURL()});
 
@@ -110,7 +110,7 @@ async function guildEmbed(msg){
         .setTitle(guild.name)
         .setThumbnail(guild.iconURL())
         .setAuthor({name:owner.user.tag, iconURL:owner.user.avatarURL()})
-        .addFields(
+        .addFields([
             {name:'人數', value: guild.memberCount.toString()},
             {name:'線上', value: members.filter(m => m.presence?.status === 'online').size.toString(), inline: true},
             {name:'閒置', value: members.filter(m => m.presence?.status === 'idle').size.toString(), inline: true},
@@ -119,7 +119,7 @@ async function guildEmbed(msg){
             {name:'VTUBER', value: roles.get('785901109071839244').members.size.toString() ,inline: true},
             {name:'Vtuber準備中', value: roles.get('811930854401638411').members.size.toString() ,inline: true},
             {name:'Vtuber幕後', value: roles.get('811516908867551282').members.size.toString() ,inline: true},
-        )
+        ])
 
     return embed;
 }
